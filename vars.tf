2bd5e932c33a6af3a18d6b35943a6693afeb820a
venkkatu1985 <46856495+venkkatu1985@users.noreply.github.com>
2019-04-09 18:09:09 +0530

    Add files via upload
    VPC terraform files

--- vars.tf
+++ vars.tf
@@ -1,27 +1,18 @@
-variable "AWS_ACCESS_KEY" {
-  default = "XXXXXX"
-}
-variable "AWS_SECRET_KEY" {
-  default = "XXXXXXX"
-}
+
 variable "AWS_REGION" {
-  default = "us-east-1"
+  default = "eu-west-1"
+}
+variable "PATH_TO_PRIVATE_KEY" {
+  default = "mykeynew"
+}
+variable "PATH_TO_PUBLIC_KEY" {
+  default = "mykeynew.pub"
 }
 variable "AMIS" {
   type = "map"
   default = {
     us-east-1 = "ami-13be557e"
     us-west-2 = "ami-06b94666"
-    eu-west-1 = "ami-0d729a60"
+    eu-west-1 = "ami-844e0bf7"
   }
-}
-
-variable "PATH_TO_PRIVATE_KEY" {
-  default = "mykey"
-}
-variable "PATH_TO_PUBLIC_KEY" {
-  default = "mykey.pub"
-}
-variable "INSTANCE_USERNAME" {
-  default = "ubuntu"
 }
